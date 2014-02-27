c844910ddefeb3df908577e216d143c8564506cf
Barry Wong <barrywong77@gmail.com>
2014-02-27 10:00:42 -0500

    Yeoman, Grunt, Bower installed and AngularJS generator and new app created

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('nycSubwayMadnessApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
